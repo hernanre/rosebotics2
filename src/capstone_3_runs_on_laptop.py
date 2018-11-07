294a8c2c3554a14340f9f5abc3def13f6843d06f
nandoltw <nandoltw@rose-hulman.edu>
2018-11-07 09:02:28 -0500

    Done

--- src/capstone_3_runs_on_laptop.py
+++ src/capstone_3_runs_on_laptop.py
@@ -53,7 +53,11 @@ import mqtt_remote_method_calls as com
 def main():
     """ Constructs and runs a GUI for this program. """
     root = tkinter.Tk()
-    setup_gui(root)
+
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
+
+    setup_gui(root,mqtt_client)
 
     root.mainloop()
     # --------------------------------------------------------------------------
@@ -63,7 +67,7 @@ def main():
     # --------------------------------------------------------------------------
 
 
-def setup_gui(root_window):
+def setup_gui(root_window,mqtt_client):
     """ Constructs and sets up widgets on the given window. """
     frame = ttk.Frame(root_window, padding=10)
     frame.grid()
@@ -75,29 +79,36 @@ def setup_gui(root_window):
     go_forward_button.grid()
 
     go_forward_button['command'] = \
-        lambda: handle_go_forward()
+        lambda: handle_go_forward(speed_entry_box, mqtt_client)
 
 
-def handle_go_forward():
+def handle_go_forward(entry_box, mqtt_client):
+    speed = entry_box.get()
+    print("Sending 'go forward' to the robot with a speed",speed)
+    mqtt_client.send_message('go forward', [speed])
+
     """
     Tells the robot to go forward at the speed specified in the given entry box.
     """
     # --------------------------------------------------------------------------
-    # TODO: 6. This function needs the entry box in which the user enters
+    # DONE: 6. This function needs the entry box in which the user enters
     # TODO:    the speed at which the robot should move.  Make the 2 changes
     # TODO:    necessary for the entry_box constructed in  setup_gui
     # TODO:    to make its way to this function.  When done, delete this TODO.
     # --------------------------------------------------------------------------
 
     # --------------------------------------------------------------------------
-    # TODO: 7. For this function to tell the robot what to do, it needs
+    # DONE: 7. For this function to tell the robot what to do, it needs
     # TODO:    the MQTT client constructed in main.  Make the 4 changes
     # TODO:    necessary for that object to make its way to this function.
     # TODO:    When done, delete this TODO.
     # --------------------------------------------------------------------------
 
+
+
+
     # --------------------------------------------------------------------------
-    # TODO: 8. Add the single line of code needed to get the string that is
+    # DONE: 8. Add the single line of code needed to get the string that is
     # TODO:    currently in the entry box.
     # TODO:
     # TODO:    Then add the single line of code needed to "call" a method on the
